8f1b75d5efd1732686f8effe0ac96553bcf8f963
LouisVelz <velazquezlouis2@gmail.com>
2020-06-13 23:24:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const rideEvent = require("./routes/api/ride_event");
+const bodyParser = require("body-parser");
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) =>{
+  console.log(res)
+  res.send("Whats up?");
+});
+app.use("/api/users", users);
+app.use("/api/rideevent", rideEvent);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
