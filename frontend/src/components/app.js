95291ebb472e4efed02a7b5190b8befdf0c24456
LouisVelz <velazquezlouis2@gmail.com>
2020-06-15 11:08:27 -0400

    working fontend

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
-import NavBarContainer from "./nav/navbar_container";
+// import NavBarContainer from "./nav/navbar_container";
 
 import MainPage from "./main/main_page";
 // import LoginFormContainer from "./session/login_form_container";
@@ -9,7 +9,7 @@ import MainPage from "./main/main_page";
 
 const App = () => (
   <div>
-    <NavBarContainer />
+    {/* <NavBarContainer /> */}
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
       {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
