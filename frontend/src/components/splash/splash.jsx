1d28b4458d1a14c3a216e50f59103bda58ee3b2a
lordrickyz <rickyzheng@ymail.com>
2020-06-15 18:14:15 -0400

    Add half of Splash Page

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import '../../stylesheets/splash/splash.scss';
+import splashTop1 from '../images/fitIndia.png';
+import splashTop2 from "../images/helpingRide.png";
+import splashTop3 from "../images/friendsRide.png";
+import splashTop4 from "../images/fingerAir.png";
+
 
 const mstp = (state) => {
 
@@ -15,44 +20,67 @@ const mdtp = dispatch => {
 class Splash extends React.Component {
   render() {
     return (
-    <section className="splash-container">
-      <section className="splash-top-content">
-        <div className="splash-top-context"> 
-            <h1>
-              Welcome to GroupRider.
-            </h1>
-            <h2>
-              A new way to socially interact with others. Where your solo rides can become group marathons!
-            </h2>
-        </div>
-        <div className="splash-top-links">
-          <Link to="/signup" id="splash-top-link">Join the Community →</Link>
-        </div>
-      </section>
+      <div className="splash-page">
+        <section className="splash-container">
+          <section className="splash-top-content">
+            <div id="splash-img-container">
+              <img
+                src={splashTop1}
+                className="splash-top-logo1"
+                alt="splash top1"
+              />
+              <img
+                src={splashTop2}
+                className="splash-top-logo2"
+                alt="splash top2"
+              />
+              <img
+                src={splashTop3}
+                className="splash-top-logo3"
+                alt="splash top3"
+              />
+              <img
+                src={splashTop4}
+                className="splash-top-logo4"
+                alt="splash top4"
+              />
+              <div className="splash-top-context">
+                <h1>GroupRider</h1>
+                <p>A New Way To Socially Interact With Others.</p>
+                <p> Where Your Solo Rides</p>
+                <p>Can Become Group Marathons!</p>
+              </div>
+            </div>
+            <div className="splash-top-links">
+              <Link to="/signup" id="splash-top-link">
+                Join the Community →
+              </Link>
+            </div>
+          </section>
 
+          <section className="splash-middle-content">
+            <div id="splash-middle-1">
+              <h2>Don't Bike Alone!</h2>
+              <p>
+                Riding alone can be dangerous, especially in cities with large
+                traffics. Join a fellow biker as you commute!
+              </p>
+              <img></img>
+            </div>
+            <div id="splash-middle-2">
+              <img></img>
+              <h2>Find Bike Events Near You!</h2>
+              <p>
+                Join a friend, join a family member, or even join a random
+                person and get to know each other through biking together.
+              </p>
+            </div>
+          </section>
 
-      <section className="splash-middle-content">
-        <div id="splash-middle-1">
-          <h2>Don't Bike Alone!</h2>
-          <p>Riding alone can be dangerous, especially in cities with large traffics. Join a fellow biker as you commute!</p>
-          <img></img>
-        </div>
-        <div id="splash-middle-2">
-          <img></img>
-          <h2>Find Bike Events Near You!</h2>
-          <p>Join a friend, join a family member, or even join a random person and get to know each other through biking together.</p>
-        </div>
-      </section>
-
-      <section className="splash-bottom-content">
-
-
-      </section>
-
-    </section>
-
-  
-    )}
+          <section className="splash-bottom-content"></section>
+        </section>
+      </div>
+    );}
 
 }
 
