865e47fc52424f094bdaf0232c39bf8283f831a7
LouisVelz <velazquezlouis2@gmail.com>
2020-06-15 10:33:03 -0400

    push

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 
-// import MainPage from "./main/main_page";
+import MainPage from "./main/main_page";
 // import LoginFormContainer from "./session/login_form_container";
 // import SignupFormContainer from "./session/signup_form_container";
 
@@ -11,7 +11,7 @@ const App = () => (
   <div>
     <NavBarContainer />
     <Switch>
-      {/* <AuthRoute exact path="/" component={MainPage} /> */}
+      <AuthRoute exact path="/" component={MainPage} />
       {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
       {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
     </Switch>
