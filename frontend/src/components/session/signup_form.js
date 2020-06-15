19685a7f4e7a3b1f4f4412cd37b593316e8df8d3
roman <admin@MacBook-Pro-9.local>
2020-06-15 18:30:59 -0400

    Add modals and change handle to username

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -6,8 +6,8 @@ class SignupForm extends React.Component {
         super(props);
         this.state = {
             email: '',
-            // username: '',
-            handleSubmit: '',
+            username: '',
+            // handleSubmit: '',
             password: '',
             password2: '',
             errors: {}
@@ -35,8 +35,8 @@ class SignupForm extends React.Component {
         e.preventDefault();
         let user = {
             email: this.state.email,
-            // username: this.state.username,
-            handle: this.state.handle,
+            username: this.state.username,
+            // handle: this.state.handle,
             password: this.state.password,
             password2: this.state.password2
         };
@@ -69,10 +69,10 @@ class SignupForm extends React.Component {
                         />
                         <br />
                         <input type="text"
-                            // value={this.state.username}
-                            // onChange={this.update('username')}
-                            value={this.state.handle}
-                            onChange={this.update('handle')}
+                            value={this.state.username}
+                            onChange={this.update('username')}
+                            // value={this.state.handle}
+                            // onChange={this.update('handle')}
                             placeholder="First and Last names"
                         />
                         <br />
