5cce9d4f4a1fb575942f6b6ab4091522ab82dba8
LouisVelz <velazquezlouis2@gmail.com>
2020-06-15 20:02:06 -0400

    resolve issue with app.js

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 // import NavBarContainer from "./nav/navbar_container";
 
