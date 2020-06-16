5ff803f8f2309bc31b3ad07197d79110acdb9f1d
LouisVelz <velazquezlouis2@gmail.com>
2020-06-16 11:20:29 -0400

    resolve issues

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,11 +2,6 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
 
-// import NavBarContainer from "./nav/navbar_container";
-
-import SplashPage from "./splash/splash_page";
-// import LoginFormContainer from "./session/login_form_container";
-// import SignupFormContainer from "./session/signup_form_container";
 import GreetingContainer from './greeting/greeting_container'
 import Modal from './modal/modal'
 import SplashPage from "./splash/splash";
@@ -14,11 +9,11 @@ import SplashPage from "./splash/splash";
 
 const App = () => (
   <div>
-    {/* <NavBarContainer /> */}
+
     <Modal /> 
-    <Route exact path="/" component={SplashPage} /> 
     <GreetingContainer />
-    <Route path="/" component={SplashPage} /> 
+    <Route exact path="/" component={SplashPage} /> 
+  
 
     <Switch>
       {/* <AuthRoute exact path="/" component={MainPage} />  */}
