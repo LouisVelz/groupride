38dd7e15e3a0db3831c0531a35cd08596866bb9f
LouisVelz <velazquezlouis2@gmail.com>
2020-06-16 15:04:29 -0400

    create keys and splash page

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://louis:HTzjE7b5gVOaPQOf@cluster-groupride-n81qj.mongodb.net/cluster-groupride?retryWrites=true&w=majority",
+
+  secretOrKey: "SVucSOIDAA",
+};
