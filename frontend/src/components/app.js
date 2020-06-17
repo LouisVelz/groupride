bb77320f8246e23b5301a678982049090a1a6847
LouisVelz <velazquezlouis2@gmail.com>
2020-06-17 10:01:32 -0400

    minor changes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
-
 import GreetingContainer from './greeting/greeting_container'
 import Modal from './modal/modal'
 import SplashPage from "./splash/splash";
 import Footer from './footer/footer'
-
-import MainPage from "./main/main_page";
-
 import RidesIndex from './rides/ride_index_container'
+import NewRideFormContainer from "./rides/new_ride_form_container";
 
-// import LoginFormContainer from "./session/login_form_container";
-// import SignupFormContainer from "./session/signup_form_container";
+
+//remember to erase window.getState form index.js
 
 
 
@@ -25,9 +22,7 @@ const App = () => (
   
 
     <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
-      {/* <AuthRoute exact path="/login" component={LoginFormContainer} /> */}
-      {/* <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
+      <ProtectedRoute exact path='/rides/new' component={NewRideFormContainer} />
       <Route exact path="/index" component={RidesIndex} /> 
     </Switch>
     <Footer /> 
