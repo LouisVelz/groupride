953e6fd30f6059bb05fafbf804133a8ee8e596cc
lordrickyz <rickyzheng@ymail.com>
2020-06-18 13:39:04 -0400

    Add Footer Page Styling

--- frontend/src/components/footer/technology.jsx
+++ frontend/src/components/footer/technology.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import '../../stylesheets/footer/footer.css'
+import '../../stylesheets/footer_pages/technology.scss'
 
 class Technology extends React.Component {
     render() {
         return (
+          <div id="footer-background">
             <section className="tech">
-                <h2 className="tech-title">Technology</h2>
-                <ul>
-                    <li>Backend: MongoDB / Express</li>
-                    <li>Frontend: React / Node.js</li>
-                    <li>Libraries/APIs: TBA (Google Maps API)</li>
-                </ul>
-                <h2 className="tech-title">Technical Challenges</h2>
-                <p className="tech-text">Using Google's Map API to locate created events 
-                    based on their set location. The ideal case 
-                    would be that the map would be able to search 
-                    events within the radius of the user's location 
-                    and show live updates for event creation. The app 
-                    will be able to use globally, as limitations would 
-                    be interface languages support.
-                </p>
+              <h2 className="tech-title">Technology</h2>
+              <ul>
+                <li>Backend: MongoDB / Express</li>
+                <li>Frontend: React / Node.js</li>
+                <li>Libraries/APIs: TBA (Google Maps API)</li>
+              </ul>
+              <h2 className="tech-title">Technical Challenges</h2>
+              <p className="tech-text">
+                Using Google's Map API to locate created events based on their
+                set location. The ideal case would be that the map would be able
+                to search events within the radius of the user's location and
+                show live updates for event creation. The app will be able to
+                use globally, as limitations would be interface languages
+                support.
+              </p>
             </section>
+          </div>
         );
     }
 }
