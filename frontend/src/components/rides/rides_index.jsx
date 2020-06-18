8a31e7f06ad29a59ed0cb72cc61a7d10b4630872
elvinv123 <elvinvalette@live.com>
2020-06-17 21:06:04 -0400

    pulled master

--- frontend/src/components/rides/rides_index.jsx
+++ frontend/src/components/rides/rides_index.jsx
@@ -14,7 +14,6 @@ class RidesIndex extends React.Component {
         this.props.fetchRides();
     }
 
-<<<<<<< HEAD
     RenderRideItems() {
         // debugger
         if (Array.isArray(this.props.rides[0])){
@@ -28,18 +27,6 @@ class RidesIndex extends React.Component {
                     ride={ride} 
                 />) }
             </>
-=======
-    ridesItem() {
-        if (Array.isArray(this.props.rides[0])){
-        return (
-            <ul>
-                {
-                    this.props.rides[0].map(ride =>
-                        <RidesIndexItem key={ride._id} currentUser={this.props.currentUser} updateRide={this.props.updateRide} ride={ride} />
-                    )
-                }
-            </ul>
->>>>>>> f21deb3f8876a55c5064579fffb7c2396d2cb7ec
         )}
     }
 
