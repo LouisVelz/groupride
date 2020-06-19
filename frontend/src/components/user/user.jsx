6e83b6209c8a53b3c9defef9636961e2ba310051
lordrickyz <rickyzheng@ymail.com>
2020-06-18 21:54:14 -0400

    Add Page Backgrounds

--- frontend/src/components/user/user.jsx
+++ frontend/src/components/user/user.jsx
@@ -37,31 +37,31 @@ class UserPage extends React.Component {
     }) 
 
     console.log(rides)
-    return(
-      <div className= 'user-main'>
-        <div className='user-info'>
-          <ul>
-            <li className="user-initial">{currentUser.username[0]}</li>
-            <li className="user-full-name">{currentUser.username}</li>
-            <div className="user-info">
-              <li className="user-email">{currentUser.email}</li>
-              <li className="user-rides">rides {titles.length}</li>
-            </div>
-            {/* <li>{this.state.user_miles}</li> */}
-            {/* <li>{currentUser.user_miles}</li>
+    return (
+      <div id="user-bg">
+        <div className="user-main">
+          <div className="user-info">
+            <ul>
+              <li className="user-initial">{currentUser.username[0]}</li>
+              <li className="user-full-name">{currentUser.username}</li>
+              <div className="user-info">
+                <li className="user-email">{currentUser.email}</li>
+                <li className="user-rides">rides {titles.length}</li>
+              </div>
+              {/* <li>{this.state.user_miles}</li> */}
+              {/* <li>{currentUser.user_miles}</li>
             <li>{currentUser.bicycle}</li>
             <li>{currentUser.skill}</li>
             <li>{currentUser.fb_link}</li>
             <li>{currentUser.rides_completed}</li> */}
-          </ul>
-        </div>
-        <div className="user-rides">
-          <ul>
-            {userRides}
-          </ul>
+            </ul>
+          </div>
+          <div className="user-rides">
+            <ul>{userRides}</ul>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
